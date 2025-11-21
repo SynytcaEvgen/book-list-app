f4dd97fda94798891c6ce4e3b3b81dda246e94f3
Yevhen Synytsia <sinizaevgen@gmail.com>
2025-11-21 04:54:38 +0200

    add changes

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import React, { type ButtonHTMLAttributes, type ReactNode } from 'react';
+import { type ButtonHTMLAttributes, type ReactNode } from 'react';
 // Import the CSS file for styles
 import './button.css';
 
